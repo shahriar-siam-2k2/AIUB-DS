a8e03829c0aab23f10cd71d64463410d5c929fb3
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-06-27 18:10:39 +0600

    DS-MID-LT-2.2-02

--- LAB Tasks/LAB Task-2.2 [Sun 23-Jun-24]/03-Matrix Input & Addition-Substration/03-TwoMatrixAddSub.cpp
+++ LAB Tasks/LAB Task-2.2 [Sun 23-Jun-24]/03-Matrix Input & Addition-Substration/03-TwoMatrixAddSub.cpp
@@ -1,44 +1,55 @@
-//DS-MID-LT-2.2-02: Develop a program that can take matrix input from a user and perform addition or subtraction.
+//DS-MID-LT-2.2-03: Develop a program that can take matrix input from a user and perform addition or subtraction.
 
 #include <iostream>
 using namespace std;
 
-int matAdd(int arr[][], int row, int col) {
-
+void matAdd(int *arr1, int *arr2, int row, int col) {
+    cout << endl << "Addition Matrix: " << endl;
+    for(int i=0 ; i<row ; i++) {
+        for(int j=0 ; j<col ; j++) {
+            cout << *((arr1 + i * col) + j) + *((arr2 + i * col) + j);
+            if(j < col-1) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
-int matSub(int arr[], int row, int col) {
-    
+void matSub(int *arr1, int *arr2, int row, int col) {
+    cout << endl << "Subtraction Matrix: " << endl;
+    for(int i=0 ; i<row ; i++) {
+        for(int j=0 ; j<col ; j++) {
+            cout << *((arr1 + i * col) + j) - *((arr2 + i * col) + j);
+            if(j < col-1) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
 
 int main() {
-    int row1, col1, row2, col2;
-    int *sum = new int;
+    int row, col;
+    int *op = new int;
 
-    cout << endl << "Enter Matrix-1 size (rowXcol): ";
-    cin >> row1 >> col1;
+    cout << endl << "Enter Matrix size (rowXcol): ";
+    cin >> row >> col;
 
-    int mat1[row1][col1];
+    int mat1[row][col], mat2[row][col];
 
-    cout << endl << "Enter integer numbers in Matrix-1 (" << row1 << "x" << col1 << ") :" << endl;
-    for(int i=0 ; i<row1 ; i++) {
-        for(int j=0 ; j<col1 ; j++) {
+    cout << endl << "Enter integer numbers in Matrix-1 (" << row << "x" << col << ") :" << endl;
+    for(int i=0 ; i<row ; i++) {
+        for(int j=0 ; j<col ; j++) {
             cin >> mat1[i][j];
         }
     }
 
-    cout << endl << "Enter Matrix-2 size (rowXcol): ";
-    cin >> row2 >> col2;
-
-    int mat2[row2][col2];
-
-    cout << endl << "Enter integer numbers in Matrix-2 (" << row2 << "x" << col2 << ") :" << endl;
-    for(int i=0 ; i<row2 ; i++) {
-        for(int j=0 ; j<col2 ; j++) {
+    cout << endl << "Enter integer numbers in Matrix-2 (" << row << "x" << col << ") :" << endl;
+    for(int i=0 ; i<row ; i++) {
+        for(int j=0 ; j<col ; j++) {
             cin >> mat2[i][j];
         }
     }
-
-    int *op = new int;
 
     while(true) {
         cout << endl << "1. Addition." << endl;
@@ -47,7 +58,17 @@ int main() {
         cin >> *op;
 
         if(*op == 1) {
-            matAdd(mat1, row1, col1);
+            matAdd(*mat1, *mat2, row, col);
+            break;
+        }
+        else if(*op == 2) {
+            matSub(*mat1, *mat2, row, col);
+            break;
+        }
+        else {
+            cout << endl << "Wrong operation." << endl;
         }
     }
+
+    delete op;
 }
