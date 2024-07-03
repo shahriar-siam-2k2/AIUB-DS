36f56c50c6ce47863b6ddccc055e5085fae03903
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-07-04 01:49:19 +0600

    DS-MID-LT-2.3-5 final

--- LAB Tasks/LAB Task-2.3 [Thu 27-Jun-24]/DS-MID-LT-2.3-5/DS-MID-LT-2.3-5.cpp
+++ LAB Tasks/LAB Task-2.3 [Thu 27-Jun-24]/DS-MID-LT-2.3-5/DS-MID-LT-2.3-5.cpp
@@ -34,13 +34,15 @@ bool credentialCheck(string id, string pass) {
     return flag;
 }
 
-void create() {
+void createUser() {
     double balance;
     string name, id, dob, accOpenDate, pass;
 
     cout << endl << "\tCreate Account" << endl;
 
     while(true) {
+        cin.ignore();
+
         cout << endl << "Enter Name: ";
         getline(cin, name);
         cout << "Enter ID: ";
@@ -66,10 +68,130 @@ void create() {
     }
 }
 
-void deleteID() {
+void login() {
     string id, pass;
 
-    cout << endl << "\tUser Delete" << endl;
+    cin.ignore();
+    
+    cout << endl << "\tUser Login" << endl;
+
+    cout << "Enter User ID: ";
+    getline(cin, id);
+    cout << "Enter Password: ";
+    getline(cin, pass);
+    
+    if(credentialCheck(id, pass) == true) {
+        cout << endl << "\t* Login Successfull!" << endl;
+        cout << endl << "User Information- " << endl;
+        cout << "Name: " << user[foundIndex].name << endl;
+        cout << "ID: " << user[foundIndex].id << endl;
+        cout << "Date of Birth: " << user[foundIndex].dob << endl;
+        cout << "Account Opening Date: " << user[foundIndex].accOpenDate << endl;
+        cout << "Password: " << user[foundIndex].pass << endl;
+        cout << "Balance: " << user[foundIndex].balance << endl;
+    }
+    else {
+        cout << endl << "\t* Wrong user ID or password." << endl;
+    }
+}
+
+void updateUser() {
+    string id, pass;
+
+    cin.ignore();
+
+    cout << endl << "\tUpdate Information" << endl;
+
+    cout << endl << "Enter User ID: ";
+    getline(cin, id);
+    cout << "Enter Password: ";
+    getline(cin, pass);
+
+    if(credentialCheck(id, pass) == true) {
+        int op;
+        double updateBlnc;
+        string updateTxt;
+        bool update = false;
+        
+        while(true) {
+            cout << endl << "1. Update Name" << endl;
+            cout << "2. Update ID" << endl;
+            cout << "3. Update Date of Birth" << endl;
+            cout << "4. Update Password" << endl;
+            cout << "5. Update Balance" << endl;
+            cout << "6. Cancel" << endl;
+
+            cout << "Choose your operation: ";
+            cin >> op;
+
+            cin.ignore();
+
+            if(op == 1) {
+                cout << endl << "Enter New Name: ";
+                getline(cin, updateTxt);
+                user[foundIndex].name = updateTxt;
+                update = true;
+                break;
+            }
+            else if(op == 2) {
+                cout << endl << "Enter New ID: ";
+                getline(cin, updateTxt);
+
+                if(duplicateCheck(updateTxt) == false) {
+                    user[foundIndex].id = updateTxt;
+                    update = true;
+                    break;
+                }
+                else {
+                    cout << endl << "\t* This user id is already exists! Try different." << endl;
+                }
+            }
+            else if(op == 3) {
+                cout << endl << "Enter New Date of Birth: ";
+                getline(cin, updateTxt);
+                user[foundIndex].dob = updateTxt;
+                update = true;
+                break;
+            }
+            else if(op == 4) {
+                cout << endl << "Enter New Password: ";
+                getline(cin, updateTxt);
+                user[foundIndex].pass = updateTxt;
+                update = true;
+                break;
+            }
+            else if(op == 5) {
+                cout << endl << "Enter New Balance: ";
+                cin >> updateBlnc;
+                user[foundIndex].balance = updateBlnc;
+                update = true;
+                break;
+            }
+            else if(op == 6) {
+                cout << endl << "\t* Cancelled Update Information" << endl;
+                break;
+            }
+            else {
+                cout << endl << "\t* INVALID OPERATION!" << endl;
+            }
+        }
+
+        if(update == true) {
+            cout << endl << "\t* Information Updated Successfully!" << endl;
+        }
+    }
+    else {
+        cout << endl << "\t* Wrong user ID or password." << endl;
+        cout << "\t* Enter correct ID and Password to update information of user." << endl;
+    }
+}
+
+void deleteUser() {
+    string id, pass;
+
+    cin.ignore();
+
+    cout << endl << "\tUser Delete" << endl << endl;
 
     cout << "Enter User ID: ";
     getline(cin, id);
@@ -81,7 +203,7 @@ void deleteID() {
         for(int i=foundIndex ; i<userC ; i++) {
             user[i] = user[i+1];
         }
-        cout << endl << "\t* User (" << id << ") and corresponding Password Deleted Successfully!" << endl;
+        cout << endl << "\t* User (" << id << ") and corresponding Data Deleted Successfully!" << endl;
     }
     else {
         cout << endl << "\t* Wrong user ID or password!" << endl;
@@ -93,29 +215,47 @@ int main() {
 
     while (true) {
         cout << endl << "1. Create Account." << endl;
-        cout << "2. Delete Account." << endl;
-        cout << "3. Exit." << endl;
+        cout << "2. Login." << endl;
+        cout << "3. Update Information." << endl;
+        cout << "4. Delete Account." << endl;
+        cout << "5. Exit." << endl;
 
         cout << "Choose your operation: ";
         cin >> op;
 
         if(op == 1) {
-            create();
+            createUser();
         }
         else if(op == 2) {
             if(userC == 0) {
-                cout << endl << "No Account Created!" << endl;
+                cout << endl << "\t* No Account Created!" << endl;
             }
             else {
-                deleteID();
+                login();
             }
         }
         else if(op == 3) {
-            cout << endl << "Successfully exited." << endl;
+            if(userC == 0) {
+                cout << endl << "\t* No Account Created!" << endl;
+            }
+            else {
+                updateUser();
+            }
+        }
+        else if(op == 4) {
+            if(userC == 0) {
+                cout << endl << "\t* No Account Created!" << endl;
+            }
+            else {
+                deleteUser();
+            }
+        }
+        else if(op == 5) {
+            cout << endl << "\t* Successfully exited." << endl;
             break;
         }
         else {
-            cout << endl << "INVALID OPERATION!" << endl;
+            cout << endl << "\t* INVALID OPERATION!" << endl;
         }
     }
 
